96e2d0586adcef1538d8c22ebb2fd2d2bdee9a2b
Alex Korkin <alexander.korkin@muranosoft.com>
2019-06-26 10:56:14 +0300

    Add holiday and vacation days

--- src/test/java/ru/piom/time/TimeTrackerTest.java
+++ src/test/java/ru/piom/time/TimeTrackerTest.java
@@ -5,6 +5,8 @@ import org.junit.Test;
 import org.openqa.selenium.By;
 
 import java.time.LocalDate;
+import java.time.Year;
+import java.time.YearMonth;
 import java.time.format.DateTimeFormatter;
 import java.util.Arrays;
 import java.util.List;
@@ -16,28 +18,27 @@ import static com.codeborne.selenide.Selenide.$;
 @Slf4j
 public class TimeTrackerTest extends FillTimeTracker {
 
-    static int month = 5;
+    private int year = Year.now().getValue();
+    private int month = 6;
+    private int maxNumberDay = YearMonth.of(year, month).atEndOfMonth().getDayOfMonth();
 
-    List<Integer> excludeDates = Arrays.asList(1, 2, 3, 9, 10);
 
-    static DateTimeFormatter dateFormat = DateTimeFormatter.ofPattern("dd.MM.yyyy");
+    private List<Integer> excludeDates = Arrays.asList(12);
+    private List<Integer> holidayDates = Arrays.asList(12);
+    private List<Integer> vacationDates = Arrays.asList();
+
+    private static DateTimeFormatter dateFormat = DateTimeFormatter.ofPattern("dd.MM.yyyy");
 
     @Test
     public void FillTimeTracker() {
         List<String> days = IntStream
-                .rangeClosed(1, 31)
+                .rangeClosed(1, maxNumberDay)
                 .mapToObj(
-                        item -> LocalDate.of(2019, month, item))
+                        item -> LocalDate.of(year, month, item))
                 .filter(cal -> cal.getDayOfWeek().getValue() < 6 && !excludeDates.contains(cal.getDayOfMonth()))
                 .map(date -> date.format(dateFormat) + " 0:00:00")
                 .collect(Collectors.toList());
-        List<String> excludeDays = IntStream
-                .rangeClosed(1, 31)
-                .mapToObj(
-                        item -> LocalDate.of(2019, month, item))
-                .filter(cal -> cal.getDayOfWeek().getValue() < 6 && excludeDates.contains(cal.getDayOfMonth()))
-                .map(date -> date.format(dateFormat) + " 0:00:00")
-                .collect(Collectors.toList());
+
         $(By.id("ctl00__content_calDatesSelector")).find(By.linkText(">>")).click();
 
         days.forEach(item -> {
@@ -51,7 +52,7 @@ public class TimeTrackerTest extends FillTimeTracker {
             $(By.id("ctl00__content_lbtnAddEntry")).click();
         });
 
-        excludeDays.forEach(item -> {
+        getDays(holidayDates).forEach(item -> {
             $(By.id("ctl00__content_ddlDays")).selectOptionByValue(item);
             $(By.id("ctl00__content_ddlProjects")).selectOptionByValue("3011");
             $(By.id("ctl00__content_ddlCategories")).selectOption("Holiday");
@@ -61,5 +62,23 @@ public class TimeTrackerTest extends FillTimeTracker {
             $(By.id("ctl00__content_tboxDescription")).val("Holiday");
             $(By.id("ctl00__content_lbtnAddEntry")).click();
         });
+        getDays(vacationDates).forEach(item -> {
+            $(By.id("ctl00__content_ddlDays")).selectOptionByValue(item);
+            $(By.id("ctl00__content_ddlProjects")).selectOptionByValue("3011");
+            $(By.id("ctl00__content_ddlCategories")).selectOptionByValue("26");
+            Integer startH = Integer.valueOf($(By.id("ctl00__content_ucStartTime_ddlHour")).val());
+            int endH = startH + 8;
+            $(By.id("ctl00__content_ucEndTime_ddlHour")).selectOption(Integer.toString(endH));
+            $(By.id("ctl00__content_tboxDescription")).val("Vacation paid");
+            $(By.id("ctl00__content_lbtnAddEntry")).click();
+        });
+    }
+
+    private List<String> getDays(List<Integer> dates) {
+        return dates.stream()
+                .map(item -> LocalDate.of(year, month, item))
+                .filter(cal -> cal.getDayOfWeek().getValue() < 6)
+                .map(date -> date.format(dateFormat) + " 0:00:00")
+                .collect(Collectors.toList());
     }
 }
